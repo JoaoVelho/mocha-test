876a8d43065a80728ccdb188d98d0261e70ea30f
Joao Velho <joaovictor.rv39@gmail.com>
2021-01-26 10:43:35 -0300

    sinon

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Math = require('../src/math');
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 let val = 0;
 
@@ -29,6 +30,23 @@ describe('Math class', function () {
     expect(math.multiply(val, 5)).to.equal(0);
   });
 
+  it.only('Calls res with sum and index values', function () {
+    const req = {};
+    const res = {
+      load: function load() {
+        console.log('Called');
+      },
+    };
+
+    sinon.spy(res, 'load');
+
+    const math = new Math();
+
+    math.printSum(req, res, 5, 5);
+
+    expect(res.load.args[0][1]).to.equal(10);
+  });
+
   it('Object', function () {
     const obj = {
       name: 'Joao Victor',
@@ -37,7 +55,7 @@ describe('Math class', function () {
     expect(obj).to.have.property('name').equal('Joao Victor');
   });
 
-  it.only('Compare objects', function () {
+  it('Compare objects', function () {
     const obj = {
       year: 2021,
     };

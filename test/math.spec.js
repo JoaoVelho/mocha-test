55835865b674d07af37c9da06951a25f37d0e536
Joao Velho <joaovictor.rv39@gmail.com>
2021-01-26 10:24:28 -0300

    chai

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,22 +1,23 @@
 const assert = require('assert');
 const Math = require('../src/math');
+const expect = require('chai').expect;
 
-let value = 0;
+let val = 0;
 
 describe('Math class', function () {
   //hooks
   beforeEach(function () {
-    value = 0;
+    val = 0;
   });
 
   it('Sum two numbers', function (done) {
     const math = new Math();
     this.timeout(3000);
 
-    value = 5;
+    val = 5;
 
-    math.sum(value, 5, value => {
-      assert.strictEqual(value, 10);
+    math.sum(val, 5, value => {
+      expect(value).to.equal(10);
       done();
     });
   });
@@ -24,6 +25,27 @@ describe('Math class', function () {
   it('Multiply two numbers', function () {
     const math = new Math();
 
-    assert.strictEqual(math.multiply(value, 5), 0);
+    // assert.strictEqual(math.multiply(val, 5), 0);
+    expect(math.multiply(val, 5)).to.equal(0);
+  });
+
+  it('Object', function () {
+    const obj = {
+      name: 'Joao Victor',
+    };
+
+    expect(obj).to.have.property('name').equal('Joao Victor');
+  });
+
+  it.only('Compare objects', function () {
+    const obj = {
+      year: 2021,
+    };
+
+    const obj2 = {
+      year: 2021,
+    };
+
+    expect(obj).to.deep.equal(obj2);
   });
 });
